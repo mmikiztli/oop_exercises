c0318e07df4892b0011667c35d1097b40d726e46
martikecskemeti <martikecskemeti@gmail.com>
2017-01-07 21:16:33 +0100

    completed

--- python-oop-si-exercises-martikecskemeti/2-100-doors/main.py
+++ python-oop-si-exercises-martikecskemeti/2-100-doors/main.py
@@ -8,7 +8,7 @@ from door import Door
 #   - count: integer - number of Door instances in the returned list (default=100)
 # Return: List of Door object instances with defined id values
 def generate_doors(count=100):
-    return []
+    return [Door(i) for i in range(1, count + 1)]
 
 
 # Does the main 100 logic. Goes through all the Door instances by the rules of 100 Doors
@@ -17,16 +17,20 @@ def generate_doors(count=100):
 #   - doors: list of Door objects
 # Return: list of toggled Door instances
 def toggle_doors(doors):
-    return []
+    for i in range(1, len(doors) + 1):
+        for j in range(i, len(doors) + 1, i):
+            if doors[j - 1].id == j:
+                Door.toggle(doors[j - 1])
+    return doors
 
 
-# Colects the ids of the open Doors form the given Door list
+# Collects the ids of the open Doors form the given Door list
 #
 # Arg:
 #   - doors: list of (preferably toggled) Door objects
 # Return: list of open Door ids as strings
 def collect_open_doors(doors):
-    return []
+    return [str(door.id) for door in doors if door.is_open == True]
 
 
 # Formats a string list into a printable string
@@ -35,7 +39,7 @@ def collect_open_doors(doors):
 #   - open_door_list: list of strings
 # Return: string - comma separated values from the string list
 def format_open_door_names(open_door_ids):
-    return ""
+    return ", ".join(open_door_ids)
 
 
 # Main logic of the 100doors example script
@@ -47,7 +51,8 @@ def main(door_count=100):
     doors = generate_doors(door_count)
     toggled_doors = toggle_doors(doors)
     open_doors = collect_open_doors(toggled_doors)
-    print("The following doors are left open: " + format_open_door_names(open_doors))
+    print("The following doors are left open: " +
+          format_open_door_names(open_doors))
 
 if __name__ == '__main__':
     main(100)
